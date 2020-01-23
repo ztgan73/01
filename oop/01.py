e39c8e69bc4824bb5f4e646b178c614b4d50a06b
ztgan73 <ztgan73@sina.cn>
2020-01-24 01:50:51 +0800

    Initial commit

---
+++ oop/01.py
@@ -0,0 +1,21 @@
+print('hello world!')
+print(1+2)
+
+class Student ():
+    pass
+mingyue = Student()
+
+class PythonStudent ():
+    name = None
+    age = 18
+    course = "Python"
+    def doHomework(self):
+        print("I 做作业")
+        return None
+yueyue = PythonStudent()
+print(yueyue.name)
+print(yueyue.age)
+yueyue.doHomework()
+
+
+
